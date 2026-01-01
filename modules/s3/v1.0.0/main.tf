a1c0267c1815ca3559ef40957759626a2a985585
Prince Akash <prince2021akash@gmail.com>
2026-01-01 18:48:25 +0530

    Automated fix from Jira intent

--- modules/s3/v1.0.0/main.tf
+++ modules/s3/v1.0.0/main.tf
@@ -20,18 +20,9 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "enc" {
 
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
+      sse_algorithm     = "aws/kms"
+      kms_master_key_id = "alias/aws/s3"
     }
-  }
-}
-
-resource "aws_s3_bucket_versioning" "versioning" {
-  for_each = { for b in var.buckets : b.name => b if b.versioning }
-
-  bucket = aws_s3_bucket.this[each.key].id
-
-  versioning_configuration {
-    status = "Enabled"
   }
 }
 
@@ -40,10 +31,10 @@ resource "aws_s3_bucket_public_access_block" "public" {
 
   bucket = aws_s3_bucket.this[each.key].id
 
-  block_public_acls   = each.value.public ? false : true
-  block_public_policy = each.value.public ? false : true
-  ignore_public_acls  = each.value.public ? false : true
-  restrict_public_buckets = each.value.public ? false : true
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 output "bucket_names" {
